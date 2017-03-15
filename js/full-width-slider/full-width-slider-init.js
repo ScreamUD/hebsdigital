0d2faa3810c83d0e908b5a7e5a4e883d6b04ed63
y.lapitsky <y.lapitsky@itransition.com>
2017-03-15 15:22:30 +0300

    finale changes

--- js/full-width-slider/full-width-slider-init.js
+++ js/full-width-slider/full-width-slider-init.js
@@ -11,20 +11,28 @@ jQuery(document).ready(function ($) {
         }
     };
 
-    var jssor_1_slider = new $JssorSlider$("full-width-slider", jssor_1_options);
+    var jssor_slider1 = new $JssorSlider$("full-width-slider", jssor_1_options);
 
     function ScaleSlider() {
-        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-        if (refSize) {
-            refSize = Math.min(refSize, 1920);
-            jssor_1_slider.$ScaleWidth(refSize);
+        var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
+        if (parentWidth) {
+            //for example minHeight is 300;
+            var minHeight = 540;
+
+            var newHeight = jssor_slider1.$OriginalHeight() / jssor_slider1.$OriginalWidth() * parentWidth;
+
+            if (newHeight < minHeight)
+                jssor_slider1.$ScaleHeight(minHeight);
+            else
+                jssor_slider1.$ScaleWidth(parentWidth);
         }
-        else {
+        else
             window.setTimeout(ScaleSlider, 30);
-        }
     }
     ScaleSlider();
+
     $(window).bind("load", ScaleSlider);
     $(window).bind("resize", ScaleSlider);
     $(window).bind("orientationchange", ScaleSlider);
+//responsive code end
 });
